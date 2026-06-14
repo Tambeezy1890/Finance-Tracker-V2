f4f02d229b2e624ad23c8a3b625a705501bb65eb
Tamboowneyi <tambowoneyi06@gmail.com>
2026-06-14 08:07:47 +0200

    Fixed the connection issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.VITE_API_URL,
+  baseURL: "https://finance-tracker-v2-production-a80e.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
